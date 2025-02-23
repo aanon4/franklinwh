85c3bf23bcb83376fc11fd8856820ec7bd84e49f
Tim Wilkinson <tim.wilkinson@me.com>
2025-02-22 17:18:03 -0800

    Add more functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// See https://github.com/richo/franklinwh-python
+
 const fetch = require("node-fetch");
 const createHash = require("node:crypto").createHash;
 const crc32 = require("crc/crc32");
@@ -86,6 +88,14 @@ class Api {
         return JSON.parse(response.result.dataArea);
     }
 
+    async _getSwitches() {
+        const response = await this._mqttSend(311, {
+            opt: 0,
+            order: this.gateway
+        });
+        return JSON.parse(response.result.dataArea);
+    }
+
     async getAGateStatus() {
         const stats = await this._getData();
         return {
@@ -116,6 +126,190 @@ class Api {
         else {
             throw new Error(json.message);
         }
+    }
+
+    async getControls() {
+        const q = new URLSearchParams({
+            id: this.gateway,
+            lang: this.lang
+        });
+        const res = await fetch(`${this.base}hes-gateway/terminal/selectTerGatewayControlLoadByGatewayId?${q}`, {
+            method: "GET",
+            headers: {
+                loginToken: this.token
+            }
+        });
+        const json = await res.json();
+        if (json.success) {
+            return json.result;
+        }
+        else {
+            throw new Error(json.message);
+        }
+    }
+
+    async getMode() {
+        const json = await this._getSwitches();
+        switch (json.runingMode) {
+            case 44609:
+                return "TOU";
+            case 37298:
+                return "SELF";
+            case 35183:
+                return "EMER";
+            default:
+                return json.runingMode;
+        }
+    }
+
+    async setMode(mode) {
+        let cmd;
+        switch (mode) {
+            default:
+                throw new Error(`Unknown mode: ${mode}`);
+            case "TOU":
+                cmd = { currendId: 44609, workMode: 1 };
+                break;
+            case "SELF":
+                cmd = { currendId: 37298, workMode: 2 };
+                break;
+            case "EMER":
+                cmd = { currendId: 35183, workMode: 3 };
+                break;
+        }
+        const current = await this._getSwitches();
+        if (current.runingMode == cmd.currendId) {
+            return true;
+        }
+        const res = await fetch(`${this.base}hes-gateway/terminal/tou/updateTouMode`, {
+            method: "POST",
+            headers: {
+                loginToken: this.token,
+                "Content-Type": "application/x-www-form-urlencoded",
+                optsource: "3"
+            },
+            body: new URLSearchParams({
+                gatewayId: this.gateway,
+                lang: this.lang,
+                oldIndex: "1",
+                stromEn: current.stromEn,
+                currendId: cmd.currendId,
+                workMode: cmd.workMode
+            })
+        });
+        const json = await res.json();
+        return json.success;
+    }
+
+    async getSmartSwitches() {
+        const json = await this._getSwitches();
+        const smart = [];
+        if (json.Sw1Name) {
+            smart.push({
+                name: json.Sw1Name,
+                id: "Sw1",
+                state: !!json.Sw1ProLoad
+            });
+        }
+        if (!json.SwMerge && json.Sw2Name) {
+            smart.push({
+                name: json.Sw2Name,
+                id: "Sw2",
+                state: !!json.Sw2ProLoad
+            });
+        }
+        if (json.Sw3Name) {
+            smart.push({
+                name: json.Sw3Name,
+                id: "Sw3",
+                state: !!json.Sw3ProLoad
+            });
+        }
+        return smart;
+    }
+
+    async updateSmartSwitches(switches) {
+        const json = await this._getData();
+        for (let i = 0; i < switches.length; i++) {
+            switch (switches[i].id) {
+                case "Sw1":
+                    switches[i].state = !!json.pro_load[0];
+                    break;
+                case "Sw2":
+                    switches[i].state = !!json.pro_load[1];
+                    break;
+                case "Sw3":
+                    switches[i].state = !!json.pro_load[2];
+                    break;
+                default:
+                    break;
+            }
+        }
+        return switches;
+    }
+
+    async setSmartSwitches(switches) {
+        if (!Array.isArray(switches)) {
+            switches = [ switches ];
+        }
+
+        const current = await this._getSwitches();
+        current.opt = 1;
+        delete current.modeChoose;
+        delete current.result;
+
+        for (let i = 0; i < switches.length; i++) {
+            switch (switches[i].id) {
+                case "Sw1":
+                    current.Sw1MsgType = 1;
+                    if (switches[i].state) {
+                        current.Sw1Mode = 1;
+                        current.Sw1ProLoad = 0;
+                        if (current.SwMerge) {
+                            current.Sw2MsgType = 1;
+                            current.Sw2Mode = 1;
+                            current.Sw2ProLoad = 0;
+                        }
+                    }
+                    else {
+                        current.Sw1Mode = 0;
+                        current.Sw1ProLoad = 1;
+                        if (current.SwMerge) {
+                            current.Sw2MsgType = 1;
+                            current.Sw2Mode = 0;
+                            current.Sw2ProLoad = 1;
+                        }
+                    }
+                    break;
+                case "Sw2":
+                    current.Sw2MsgType = 1;
+                    if (switches[i].state) {
+                        current.Sw2Mode = 1;
+                        current.Sw2ProLoad = 0;
+                    }
+                    else {
+                        current.Sw2Mode = 0;
+                        current.Sw2ProLoad = 1;
+                    }
+                    break;
+                case "Sw3":
+                    current.Sw3MsgType = 1;
+                    if (switches[i].state) {
+                        current.Sw3Mode = 1;
+                        current.Sw3ProLoad = 0;
+                    }
+                    else {
+                        current.Sw3Mode = 0;
+                        current.Sw3ProLoad = 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        const json = await this._mqttSend(311, current);
+        return json.success;
     }
 
 }
